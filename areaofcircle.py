41e994dc527a6c3dffc3adb5bc38df82f01b567f
AnupPrabhu2004 <135731139+AnupPrabhu2004@users.noreply.github.com>
2023-06-21 11:55:36 +0530

    areaofcircle.py

--- areaofcircle.py
+++ areaofcircle.py
@@ -2,3 +2,21 @@ import math as M
 Radius=float(input("please enter the radius of circle:"))
 area_of_circle=M.pi*Radius*Radius
 print("The area of the given circle is:",area_of_circle)
+def get_file_extension(filename):
+    parts = filename.split(".")
+    
+    if len(parts) > 1 and parts[0] != "" and parts[-1] != "":
+        return parts[-1]
+    else:
+        return ""
+
+# Accept a filename from the user
+filename = input("Enter a filename: ")
+
+# Get the extension of the filename
+extension = get_file_extension(filename)
+
+if extension:
+    print("The extension of the file is:", extension)
+else:
+    print("No extension found.")
